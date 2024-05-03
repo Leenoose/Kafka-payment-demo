a61d6463ea3dff08fa1ebb981fc4bc03630ebaba
Linus <elfzity@hotmail.com>
2024-05-03 15:20:56 +0800

    Updated datatype for transaction table

--- python-backend/transaction/app/api.py
+++ python-backend/transaction/app/api.py
@@ -18,8 +18,8 @@ async def init_db():
     create_table_query = f"""
                         create table if not exists transactions (
                             transaction_id SERIAL PRIMARY KEY,
-                            sender_id SERIAL NOT NULL,
-                            recipient_id SERIAL NOT NULL,
+                            sender_id INTEGER NOT NULL,
+                            recipient_id INTEGER NOT NULL,
                             amount NUMERIC(7,5) NOT NULL
                         );
                         """
