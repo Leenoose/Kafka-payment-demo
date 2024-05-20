c5b4879263986a458af89ffca8bae7337fd767fd
Linus <elfzity@hotmail.com>
2024-05-20 15:28:19 +0800

    Updated containerfile port

--- python-backend/balance/db_init.sql
+++ python-backend/balance/db_init.sql
@@ -1,4 +1,8 @@
+--psql -c "<command>"
 create table if not exists balances (
 user_id INTEGER PRIMARY KEY,
 balance DECIMAL(10,2) NOT NULL
 );
+
+insert into balances (user_id, balance)
+values (1, 100.00), (2, 200.00)
